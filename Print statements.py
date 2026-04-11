7101751e63b70048aa06d61e4d2fa36cc6e6b84b
pihusanghai12-design <pihusanghai12@gmail.com>
2026-04-11 12:23:54 +0545

    Print statements

---
+++ Print statements.py
@@ -0,0 +1,6 @@
+print("Python ia a high level programming language")
+print("It is an interpreted, object-oriented based")
+print("Python is used in machine learning, web development, data science, data visualisation, and game development.")
+print("It was developed by Guido Van Rossum in 1991")
+#Comments in Python are used to explain the code but it is not printed
+'''My name is Pihu''' #Second way of comments

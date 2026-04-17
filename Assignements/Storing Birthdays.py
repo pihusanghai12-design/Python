596a6126f0bdf2df2054f5e973e7eebcc54d1183
pihusanghai12-design <pihusanghai12@gmail.com>
2026-04-17 19:05:00 +0545

    Storing Birthdays

---
+++ Assignements/Storing Birthdays.py
@@ -0,0 +1,11 @@
+mom = input("Mom: Enter your birthday: ")              # lowercase
+DAD = input("Dad: Enter your birthday: ")              # uppercase
+brother_name = input("Brother: Enter your birthday: ") # underscore
+_friend = input("Friend: Enter your birthday: ")       # leading underscore
+bestfriend1 = input("Bestfriend 1: Enter your birthday: ") # number at end
+
+print("Mom's birthday is:", mom)
+print("Dad's birthday is:", DAD)
+print("Brother's birthday is:", brother_name)
+print("Friend's birthday is:", _friend)
+print("Bestfriend1's birthday is:", bestfriend1)

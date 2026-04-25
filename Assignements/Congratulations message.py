f2af40a0d7b66f66223d576e0f01d9b75065b8cd
pihusanghai12-design <pihusanghai12@gmail.com>
2026-04-25 19:31:30 +0545

    Congratulations message

---
+++ Assignements/Congratulations message.py
@@ -0,0 +1,12 @@
+Congratulations_1="Congratulations for your Acheivements"
+print(len(Congratulations_1))
+
+Congratulations_2="Congratulations for your victory"
+print(Congratulations_2[12])
+
+Congratulations_3="Congratulations on your result"
+print(Congratulations_3[::-1])
+
+Congratulations_4="Congratulations for your award."
+Sentence=" Hope you have a bright future."
+print(Congratulations_4 + Sentence)
---
+++ Operators in Python/Assignment Operators.py
@@ -0,0 +1,15 @@
+a=7
+a *=9    #a=a*9
+print(a)
+
+b=7
+b +=9    
+print(b)
+
+c=17
+c -=9    
+print(c)
+
+d=14
+d /=2  
+print(d)

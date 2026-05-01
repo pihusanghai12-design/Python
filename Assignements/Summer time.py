eea35f6619f49f609857c2fa6fdeccb476e34c5a
pihusanghai12-design <pihusanghai12@gmail.com>
2026-05-01 19:00:00 +0545

    summer time

---
+++ Assignements/Summer time.py
@@ -0,0 +1,7 @@
+temperature=int(input("Enter the temperature of your area: "))
+if temperature > 30:
+    print(" Summer- Hydrate before you feel thirsty. If you wait till you’re thirsty, you’re already slightly dehydrated.")
+elif temperature < 20:
+    print(" Winter- Dress in layers, not one thick jacket. You can adjust easily instead of freezing or overheating.")
+else:
+    print("Neutral- Don’t get careless—carry a light layer (like a hoodie). Weather can flip fast, especially evenings.")        

acc8f00b1afdfd030b1b5414e4ea668cf35e135a
pihusanghai12-design <pihusanghai12@gmail.com>
2026-05-02 12:26:18 +0545

    Logical Operators

---
+++ Logical Operators/And and Or operator.py
@@ -0,0 +1,17 @@
+#And operator
+a=1
+b=0
+c=1
+if a>b and b<c:
+    print("Both the conditions are True.")
+else:
+    print("Atleast one of the condition is false.")    
+
+#Or operator
+a=1
+b=0
+c=0
+if a>0 or b>0 :
+    print("Any one condition is True.")
+else: 
+    print("Both are conditions are False or True.")    
---
+++ Logical Operators/BMI calculator.py
@@ -0,0 +1,18 @@
+height=float(input("Enter your height in cm: "))
+weight=float(input("Enter your weight in kg: "))
+
+BMI= weight/(height/100)**2
+
+print("Your BMI is: ", BMI)
+if BMI <=18.4:
+    print("Underweight")
+elif BMI <=24.9:
+    print("Healthy") 
+elif BMI <=29.9:
+    print("Overweight") 
+elif BMI <=34.9:
+    print("Severely overweight") 
+elif BMI <=39.9:
+    print("Obese")
+else:
+    print("Severly Obese")                    

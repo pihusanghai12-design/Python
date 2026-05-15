90f7ad8ae155ba8e2693560d1280ede7d4ccc3d6
pihusanghai12-design <pihusanghai12@gmail.com>
2026-05-15 18:57:28 +0545

    Checking age

---
+++ Assignements/Checking age.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your age: "))
+
+if age >= 0:
+    
+    if age < 13:
+        print("You are minor.")
+        
+    elif age < 18:
+        print("You are Teenager.")
+        
+    else:
+        print("You are an adult.")
+        
+

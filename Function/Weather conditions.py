5e1e8908674a3c5e65aebc1f87694e2f8346d760
pihusanghai12-design <pihusanghai12@gmail.com>
2026-06-12 18:24:55 +0545

    Function

---
+++ Function/Weather conditions.py
@@ -0,0 +1,6 @@
+def weather_conditions():
+    print("The weather is pleasant in",autumn)
+    print("The weather is same in",spring)
+autumn="spring"
+spring="autumn"    
+weather_conditions()
---
+++ Function/Wellwishes.py
@@ -0,0 +1,4 @@
+def wellwishes():  #Defination of function
+    print("Have a great day.")
+    print("May god bless you.")
+wellwishes()    #declaring/calling of function

c87c81c5a65b08b7bcccdb45518ec75ada11d821
pihusanghai12-design <pihusanghai12@gmail.com>
2026-07-04 12:29:29 +0545

    List

---
+++ Lists/Oerations of lists.py
@@ -0,0 +1,32 @@
+#Create an Empty List
+empty_list= []
+print()
+
+#Length Of the lsit
+a="Python"
+print(len(a))
+
+#Slicing in strings
+s="Today's Weather is Vibrant."
+print(s[9])
+print(s[0:5])
+print(s[:6])
+print(s[5:])
+
+#Concatenation of Strings
+g="Good "
+m="Morning "
+print(g+m)
+
+#A list of Numbers
+p= [3,"Hello", 8.9, True]
+print(p)
+
+#Use * Operator
+triples= [2,5,7] * 3
+print(triples)
+
+#Reverse the list
+List= [200,666,999,454]
+List= List[::-1]
+print(List)
---
+++ Lists/Word Matching.py
@@ -0,0 +1,12 @@
+def word_match(words):
+    ctr= 0
+    lst= []
+    for word in words:
+        if len(word)> 1 and word[0] == word[-1]:
+            ctr +=1
+            lst.append(word)
+    print("List of words with the first and the last letter same are: ",lst)
+    return ctr
+
+count= word_match(["lil","ctrc","1231","Tea","abc"])       
+print("Number of words having first and last letter same: ",count)

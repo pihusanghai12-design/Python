294d60c5413feb5618c0d254388b93d8e58c1dad
pihusanghai12-design <pihusanghai12@gmail.com>
2026-08-22 08:51:15 +0545

    Random Password

--- Assignements/Random PAssword.py
+++ Assignements/Random PAssword.py
@@ -2,4 +2,4 @@ Password= input("Enter a random password: ")
 if Password.upper():
     print("Password is in Uppercase")
 else:
-    print("Password is in Lowercase")    
+    print("Password is in Lowercase")   
